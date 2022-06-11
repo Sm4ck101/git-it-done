dfe7d3ce8d0815f47cc95f296267b23827ab4b2a
Sm4ck <mikeamartinez314@gmail.com>
2022-06-11 15:35:30 -0500

    search by language feature added

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormE1 = document.querySelector("#user-form");
 var nameInputE1 = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -75,3 +76,30 @@ var displayRepos = function(repos, searchTerm)  {
         repoContainerEl.appendChild(repoEl);
     }
 };
+
+var getFeaturedRepos = function(language)    {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response)   {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });   
+        }   else    {
+            alert("Error: Github User Not Found");
+        }
+    })
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+    if (language)   {
+        getFeaturedRepos(language);
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+} 
+
+
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
